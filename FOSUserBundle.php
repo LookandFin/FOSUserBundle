6affba5d69b1cb59c1e8353d76aca735f8e2c129
Romain <rn@lookandfin.com>
2026-02-16 08:52:43 +0100

    Update FOSUserBundle.php

--- FOSUserBundle.php
+++ FOSUserBundle.php
@@ -31,7 +31,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class FOSUserBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new ConfigurePasswordHasherPass());
